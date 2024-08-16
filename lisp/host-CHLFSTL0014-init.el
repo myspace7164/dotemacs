bce4e4b536cb933bc41b1571835c1af1da1e6db8
Bruun Alexander <a.bruun@freyag-stans.ch>
2024-08-16 16:03:54 +0200

    Change org dir for work laptop

--- lisp/host-CHLFSTL0014-init.el
+++ lisp/host-CHLFSTL0014-init.el
@@ -1,4 +1,4 @@
 (setq my/system-theme 'modus-operandi)
-(setq my/org-directory "z:/org/")
+(setq my/org-directory "c:/Users/Abru/Nextcloud/org/")
 (setq my/browse-url-browser-function 'browse-url-default-browser)
 (setq my/default-font "Iosevka-10")

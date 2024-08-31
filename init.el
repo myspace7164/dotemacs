bb7707ba48192653a2fee808488fc3c09921ba36
Alexander Bruun <alex@bruun.xyz>
2024-08-31 09:51:15 +0200

    Add org-caldav

--- init.el
+++ init.el
@@ -523,7 +523,7 @@
          (org-mode . visual-line-mode))
   :config
   (setq org-directory my/org-directory)
-  (setq org-agenda-files (list org-directory))
+  (setq org-agenda-files (list org-directory "~/.local/share/org"))
   (setq org-default-notes-file (concat org-directory "notes.org"))
 
   (setq org-special-ctrl-k t)
@@ -605,6 +605,15 @@
 ;; (use-package org-fragtog
 ;;   :ensure t
 ;;   :hook (org-mode . org-fragtog-mode))
+
+(use-package org-caldav
+  :ensure t
+  :config
+  (setq org-caldav-url "https://criteria8905.ddns.net/remote.php/dav/calendars/disrupt9645")
+  (setq org-caldav-calendar-id "personal")
+  (setq org-caldav-inbox "~/.local/share/org/calendar.org")
+
+  (run-at-time nil (* 5 60) 'org-caldav-sync))
 
 (use-package org-faces
   :after org
